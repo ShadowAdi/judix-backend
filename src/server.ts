907803a704183ea77519be15771a01f3025b48d5
ShadowAdi <shadowshukla76@gmail.com>
2026-01-08 02:28:44 +0530

    added auth router

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { CustomErrorHandler } from "./middlewares/CustomErrorHandler.js";
 import { healthRouter } from "./routes/healthRouter.js";
 import { userRouter } from "./routes/userRouter.js";
 import { caseRouter } from "./routes/caseRouter.js";
+import { authRouter } from "./routes/authRouter.js";
 
 
 const app = express()
@@ -14,6 +15,7 @@ app.use(express.json())
 
 app.use("/api/v1/health", healthRouter)
 app.use("/api/v1/user", userRouter)
+app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/case", caseRouter)
 
 app.use(CustomErrorHandler);
