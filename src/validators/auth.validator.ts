71ac452b44066aa534480838e18a24c8c2f6d0c2
ShadowAdi <shadowshukla76@gmail.com>
2026-01-07 19:47:55 +0530

    Added Validator

---
+++ src/validators/auth.validator.ts
@@ -0,0 +1,11 @@
+import { body } from "express-validator/";
+
+export const loginUserValidator = [
+  body("email")
+    .notEmpty().withMessage("Email is required")
+    .isEmail().withMessage("Invalid email")
+    .normalizeEmail(),
+
+  body("password")
+    .notEmpty().withMessage("Password is required"),
+];
---
+++ src/validators/user.validator.ts
@@ -0,0 +1,39 @@
+import { body } from "express-validator/";
+
+export const createUserValidator = [
+    body("username")
+        .notEmpty().withMessage("Username is required")
+        .isLength({ min: 3, max: 30 }).withMessage("Username must be 3–30 characters")
+        .matches(/^[a-zA-Z0-9_]+$/).withMessage("Username can only contain letters, numbers, and underscores")
+        .trim(),
+
+    body("email")
+        .notEmpty().withMessage("Email is required")
+        .isEmail().withMessage("Invalid email format")
+        .normalizeEmail(),
+
+    body("password")
+        .notEmpty().withMessage("Password is required")
+        .isLength({ min: 3 }).withMessage("Password must be at least 8 characters"),
+
+    body("bio")
+        .optional()
+        .isLength({ max: 200 }).withMessage("Bio cannot exceed 200 characters")
+        .trim(),
+];
+
+export const updateUserValidator = [
+  body("username")
+    .optional()
+    .isLength({ min: 3, max: 30 })
+    .withMessage("Username must be 3–30 characters")
+    .matches(/^[a-zA-Z0-9_]+$/)
+    .withMessage("Username can only contain letters, numbers, and underscores")
+    .trim(),
+
+  body("bio")
+    .optional()
+    .isLength({ max: 200 })
+    .withMessage("Bio cannot exceed 200 characters")
+    .trim(),
+];
