7cc20572f15c9e5bc20de442416e40a400e5f450
ShadowAdi <shadowshukla76@gmail.com>
2026-01-07 20:27:52 +0530

    added user router

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,3 +1,26 @@
 import express from "express";
+import { createUserValidator, updateUserValidator } from "../validators/user.validator.js";
+import { validate } from "../middlewares/validator.js";
+import { CreateUser, DeleteUser, GetAllUsers, GetUser, UpdateUser } from "../controllers/user.controller.js";
 
 export const userRouter = express.Router();
+
+userRouter.post(
+    "/",
+    createUserValidator,
+    validate,
+    CreateUser
+);
+
+userRouter.get("/", GetAllUsers);
+
+userRouter.get("/:id", GetUser);
+
+userRouter.patch(
+    "/:id",
+    updateUserValidator,
+    validate,
+    UpdateUser
+);
+
+userRouter.delete("/:id", DeleteUser);
