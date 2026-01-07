51870484754f9413b182331bd25eca9c0cad60ff
ShadowAdi <shadowshukla76@gmail.com>
2026-01-07 21:20:10 +0530

    implemented jwt here

--- src/config/DotenvConfig.ts
+++ src/config/DotenvConfig.ts
@@ -5,3 +5,4 @@ configDotenv()
 export const PORT=process.env.PORT
 export const DB_URL=process.env.MONGODB_URL
 export const CLIENT_URL=process.env.CLIENT_URL
+export const JWT_KEY=process.env.JWT_KEY
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,11 +5,17 @@ import { UserService } from "../service/user.service.js"
 import { logger } from "../config/Logger.js"
 import { AppError } from "../utils/AppError.js"
 import jwt from "jsonwebtoken"
+import { JWT_KEY } from "../config/DotenvConfig.js"
 
 export const LoginUser = CustomTryCatch(async (request: Request, response: Response) => {
     const { email, password } = request.body
 
     const userFound = await UserService.findUserWithEmail(email)
+
+    if (!JWT_KEY) {
+        logger.error(`Failed to get JWT key generator`);
+        throw new AppError(`Internal Server error`, 500)
+    }
 
     const isPasswordCorrect = await verifyPassword(password, userFound.password)
 
@@ -18,17 +24,22 @@ export const LoginUser = CustomTryCatch(async (request: Request, response: Respo
         throw new AppError(`Invalid Creedentials`, 401)
     }
 
-    jwt.sign({
+    const token = await jwt.sign({
         "email": userFound.email,
         "id": userFound._id
-    }, "", {
+    }, JWT_KEY, {
         algorithm: "HS256",
         expiresIn: "24h"
     })
 
 
-    response.status(201).json({
+    response.status(200).json({
         "success": true,
-        "message": "User Created Successfully"
+        "message": "User Logged In Successfully",
+        "token": token,
+        "user": {
+            "email": userFound.email,
+            "id": userFound._id
+        }
     })
 })
