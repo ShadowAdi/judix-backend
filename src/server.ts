cc6e593589830307b6c45e70d53c16e9adafcfec
ShadowAdi <shadowshukla76@gmail.com>
2026-01-07 19:01:19 +0530

    adding a simple api

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { CorsConfig } from "./config/CorsConfig.js";
 import { AppConnect } from "./config/AppConfig.js";
 
@@ -8,5 +8,11 @@ const app = express()
 CorsConfig(app)
 app.use(express.json())
 
+app.get("/", (request: Request, response: Response) => {
+    return response.status(200).json({
+        "message": "API are working",
+        "success": true
+    })
+})
 
 AppConnect(app)
