d273503745ad47553e3d6479a8968006d06452ad
ShadowAdi <shadowshukla76@gmail.com>
2026-01-08 02:09:28 +0530

    implemented case controller

--- src/controllers/case.controller.ts
+++ src/controllers/case.controller.ts
@@ -49,21 +49,30 @@ export const GetUserCases = CustomTryCatch(async (request: Request, response: Re
 
 export const GetCase = CustomTryCatch(async (request: Request, response: Response) => {
     const { id } = request.params
-    const user = await UserService.getUser(id)
-
-    response.status(200).json({
-        "success": true,
-        "message": "User Fetched Successfully",
-        "user": user
-    })
-})
-
-export const DeleteUser = CustomTryCatch(async (request: Request, response: Response) => {
     if (!request.user) {
         throw new AppError('User not authenticated', 401);
     }
 
-    const deleteResponse = await UserService.deleteUser(request.user.id)
+    const user = await UserService.getUser(request.user.id);
+    const caseFound = await CaseService.getCase(id, user._id)
+
+
+    response.status(200).json({
+        "success": true,
+        "message": "Case Fetched Successfully",
+        "case": caseFound
+    })
+})
+
+export const DeleteCase = CustomTryCatch(async (request: Request, response: Response) => {
+    const { id } = request.params
+
+    if (!request.user) {
+        throw new AppError('User not authenticated', 401);
+    }
+    const user = await UserService.getUser(request.user.id);
+    const deleteResponse = await CaseService.deleteCase(id, user._id)
+
 
     response.status(200).json({
         "success": true,
@@ -72,13 +81,17 @@ export const DeleteUser = CustomTryCatch(async (request: Request, response: Resp
 })
 
 export const UpdateUser = CustomTryCatch(async (request: Request, response: Response) => {
+        const { id } = request.params
+
     if (!request.user) {
         throw new AppError('User not authenticated', 401);
     }
+        const user = await UserService.getUser(request.user.id);
 
-    const updateUser = request.body
 
-    const updateResponse = await UserService.updateUser(request.user.id, updateUser)
+    const updateCase = request.body
+
+    const updateResponse = await CaseService.updateCase(id,updateCase,request.user.id)
 
     response.status(200).json({
         "success": true,
